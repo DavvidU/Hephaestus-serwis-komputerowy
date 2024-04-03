0c8c144ac29d467fb67bc265f450d0276614939a
Dawid Ugniewski <80782@student.pb.edu.pl>
2024-04-03 23:54:19 +0200

    Dodanie konktestu bazy danych i ustalenie connection stringa

--- Hephaestus.Server/Program.cs
+++ Hephaestus.Server/Program.cs
@@ -1,4 +1,10 @@
+using Hephaestus.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HephaestusDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("HephaestusContext")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
