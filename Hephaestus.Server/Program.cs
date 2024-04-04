14a3ed0538aa56c54d01c6d422408f365dbf8854
Dawid Ugniewski <80782@student.pb.edu.pl>
2024-04-04 20:28:32 +0200

    Usunięcie artefaktu

--- Hephaestus.Server/Program.cs
+++ Hephaestus.Server/Program.cs
@@ -96,8 +96,3 @@ app.MapDelete("/failures/{id}", async (int id, HephaestusDbContext hephaestusDbC
 app.MapFallbackToFile("/index.html");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
